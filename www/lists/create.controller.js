7962e816564945b6d0cf0d61670af2c1dd60d0e6
Eduardo Santos <e3duardo@live.com>
2019-03-13 11:32:10 -0300

    ?

---
+++ www/lists/create.controller.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('CreateListController', CreateListController);
+
+    CreateListController.$inject = ['ListService', '$location', '$rootScope', 'FlashService'];
+    function CreateListController(ListService, $location, $rootScope, FlashService) {
+        var vm = this;
+
+        vm.register = register;
+        
+        initController();
+        
+
+
+        function initController() {
+            $('.color').colorpicker();
+        }
+
+
+        function register() {
+            vm.dataLoading = true;
+            ListService.Create(vm.list)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        $location.path('/lists');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+    }
+})();
